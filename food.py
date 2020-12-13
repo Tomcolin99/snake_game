f6b13e62253479d311099688a311958d82b24551
Tom Colin <tomcolin@MacBook-Pro-de-Tom.local>
2020-12-13 19:26:44 +0100

    add high score wich save on a txt file

--- food.py
+++ food.py
@@ -8,12 +8,10 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(0.5, 0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
-        self.goto(random_x, random_y)
+        self.refresh()
 
     def refresh(self):
         random_x = random.randint(-280, 280)
--- scoreboard.py
+++ scoreboard.py
@@ -1,26 +1,40 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Courier", 12, "normal")
-GAME_OVER_FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 24, "normal")
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as file:
+            self.high_score = file.read()
         self.color("white")
-        self.hideturtle()
         self.penup()
-        self.goto(0, 280)
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
 
-    def write_score(self):
-        self.write(f"Score : {self.score}", False, align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score : {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
-        self.write("GAME OVER", False, align=ALIGNMENT, font=GAME_OVER_FONT)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
-    def update_score(self):
+    def increase_score(self):
         self.score += 1
         self.clear()
-        self.write_score()
+        self.update_scoreboard()
+
+
